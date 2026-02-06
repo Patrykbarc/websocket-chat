5c0b8188c109cb2cc9b26ba79c9cfc65778733f0
Patryk Barć <patrykbarc.design@gmail.com>
2026-02-06 17:11:28 +0100

    feat: update WebSocket server implementation and enhance client messaging

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,22 +1,43 @@
 import { json, urlencoded } from 'body-parser'
 import cors from 'cors'
-import express, { type Express } from 'express'
+import { format } from 'date-fns'
+import express from 'express'
+import { createServer as createHttpServer } from 'http'
 import morgan from 'morgan'
+import { WebSocketServer } from 'ws'
 
-export const createServer = (): Express => {
+export const createServer = () => {
   const app = express()
+  const httpServer = createHttpServer(app)
+  const wss = new WebSocketServer({ server: httpServer })
+
   app
     .disable('x-powered-by')
     .use(morgan('dev'))
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get('/message/:name', (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` })
-    })
-    .get('/status', (_, res) => {
-      return res.json({ ok: true })
+
+  wss.on('connection', (ws) => {
+    console.log('Client connected')
+
+    ws.on('message', (data) => {
+      console.log('Received:', data)
+
+      const dataStr = data.toString('utf-8')
+      const timestamp = format(new Date(), 'HH:mm:ss')
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === 1) {
+          client.send(JSON.stringify({ timestamp, data: dataStr }))
+        }
+      })
     })
 
-  return app
+    ws.on('close', () => {
+      console.log('Client disconnected')
+    })
+  })
+
+  return httpServer
 }
--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -1,5 +1,53 @@
-function App() {
-  return <div>Hello world</div>
-}
+import { useWs } from '../hooks/useWs'
 
+const DEFAULT_MESSAGE_LENGTH_LIMIT = 200
+
+function App() {
+  const { input, messages, sendMessage, setInput } = useWs()
+
+  return (
+    <div className="w-full h-dvh flex p-4 md:p-8">
+      <div className="lg:w-3/4 xl:w-1/2 w-full mx-auto h-full border p-4 flex flex-col">
+        <div className="flex-1 overflow-y-auto border p-6">
+          {messages.map((message, index) => (
+            <p className="break-all" key={index}>
+              {message}
+            </p>
+          ))}
+        </div>
+        <div className="flex flex-col gap-2 pt-4">
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+                sendMessage()
+              }
+            }}
+            placeholder="Type a message"
+            className="border w-full h-24 p-4 resize-none"
+          />
+          <button
+            className="border px-4 py-1 cursor-pointer bg-gray-200"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send
+          </button>
+          <div className="flex justify-between">
+            <p className="text-red-600">
+              {input.length >= DEFAULT_MESSAGE_LENGTH_LIMIT &&
+                `Message cannot exceed ${DEFAULT_MESSAGE_LENGTH_LIMIT} characters.`}
+            </p>
+
+            <p>
+              {input.length}/{DEFAULT_MESSAGE_LENGTH_LIMIT}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default App
